f6a9524cc2ff60b1465d449baf26b11abd4b6c33
Angel Martinez <amartinr90@hotmail.com>
2023-02-23 09:39:06 -0600

    Create hook.php

---
+++ hook.php
@@ -0,0 +1,15 @@
+<?php
+function plugin_workorderprint_install() {
+   //do some stuff like instanciating databases, default values, ...
+   return true;
+}
+
+/**
+ * Uninstall hook
+ *
+ * @return boolean
+ */
+function plugin_workorderprint_uninstall() {
+   //to some stuff, like removing tables, generated files, ...
+   return true;
+}
